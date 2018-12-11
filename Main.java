e96157f99c1cfc4d1ea34236bfcc4e31d6e0b2a9
nico <nicolauria@outlook.com>
2018-12-11 15:45:22 -0800

    add java methods txt

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) {
+    int[] arr1 = {1, 2, 3};
+    int[] arr2 = {1, 2, 3};
+    System.out.println(arr1.equals(arr2));
+  }
+}
