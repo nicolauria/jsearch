d372d302906101ea08bd6d91d9624cd825111ca9
nico <nicolauria@outlook.com>
2018-12-11 18:51:28 -0800

    hash map definition

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Main {
   public static void main(String[] args) {
-    int num1 = 10;
-    int num2 = 20;
-    System.out.println(Math.abs(num1 - num2));
+    if (!null) {
+      System.out.println("hello");
+    }
   }
 }
